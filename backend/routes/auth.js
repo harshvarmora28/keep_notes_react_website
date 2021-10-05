8f2bf335bfaeaefe66a834d56908755bac497a16
Harsh Varmora <harsh.vce20@sot.pdpu.ac.in>
2021-10-05 22:27:37 +0530

    created thunder client collections to manage requests

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -4,27 +4,38 @@ const router = express.Router();
 const { body, validationResult } = require('express-validator');
 
 
-// Create a user using: POST "/api/auth", Doesn't require Auth
-router.post("/", [
+// Create a user using: POST "/api/auth/createuser", No Login Required
+router.post("/createuser", [
     body("name", "Enter a valid name").isLength({ min: 3 }),
     body("email", "Enter a valid email").isEmail(),
     body("password", "Password must be atleast 5 characters").isLength({ min: 5 })
 
 
-], (req, res) => {
+], async (req, res) => {
+    // If there are errors return Bad requests and the errors
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
         return res.status(400).json({ errors: errors.array() });
     }
 
-    User.create({
-        name: req.body.name,
-        email: req.body.email,
-        password: req.body.password,
-    }).then(user => res.json(user))
-    .catch(err => {console.log(err)
-    res.json({error: "Please enter a unique email", message: err.message})});
-    
+    // Check whether the same email exists already
+    try{
+        let user = await User.findOne({email: req.body.email});
+        if(user){
+            return res.status(400).json({error: "User with this email already exists, please choose another email"})
+        }
+        user = await User.create({
+            name: req.body.name,
+            email: req.body.email,
+            password: req.body.password,
+        })
+        
+        res.json(user);
+    }
+    catch(error){
+        console.error(error.message);
+        res.status(500).send("We are sorry, we ran into an issue!😓");
+    }
 
 });
 
