b7de53127068e853a709912aaa6a0f78d0c2e60f
Harsh Varmora <harsh.vce20@sot.pdpu.ac.in>
2021-10-09 01:03:15 +0530

    created hash passwords using bcryptjs with salt and generated authToken with jwt

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,6 +2,10 @@ const express = require("express");
 const User = require("../models/User");
 const router = express.Router();
 const { body, validationResult } = require('express-validator');
+const bcrypt = require("bcryptjs");
+var jwt = require("jsonwebtoken");
+
+const JWT_SECRET = "@harsh__creations";
 
 
 // Create a user using: POST "/api/auth/createuser", No Login Required
@@ -24,13 +28,27 @@ router.post("/createuser", [
         if(user){
             return res.status(400).json({error: "User with this email already exists, please choose another email"})
         }
+
+        const salt = await bcrypt.genSalt(10);
+        const secPass = await bcrypt.hash(req.body.password, salt);
+
+        // Create a new user
         user = await User.create({
             name: req.body.name,
             email: req.body.email,
-            password: req.body.password,
-        })
+            password: secPass
+        });
+
+        const data = {
+            user:{
+                id: user.id
+            }
+    }
+
+        const authToken = jwt.sign(data, JWT_SECRET);
+
         
-        res.json(user);
+        res.json({ authToken });
     }
     catch(error){
         console.error(error.message);
