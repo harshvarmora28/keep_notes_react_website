163bd273d55cae81066d2a2cf4c7c0f110ed0b6c
Harsh Varmora <harsh.vce20@sot.pdpu.ac.in>
2021-10-09 14:54:48 +0530

    made a login endpoint and send auth token

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -57,4 +57,45 @@ router.post("/createuser", [
 
 });
 
+// Authentication of a user using: POST "/api/auth/login". No login required
+router.post("/login", [
+    body("email", "Enter a valid email").isEmail(),
+    body("password", "Password cannot be blank").exists()
+], async (req, res) => {
+
+    // If there are errors return Bad requests and the errors
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { email, password } = req.body;
+
+    try{
+        let user = await User.findOne({email});
+        if(!user){
+            return res.status(400).json({error: "Incorrect Email or Password!"});
+        }
+        
+        const passwordCompare = await bcrypt.compare(password, user.password);
+        if(!passwordCompare){
+            return res.status(400).json({error: "Incorrect Email or Password!"});
+        }
+
+        const data = {
+            user:{
+                id: user.id
+            }
+        }
+        const authToken = jwt.sign(data, JWT_SECRET);
+        res.json({ authToken });
+
+    } catch(error){
+        console.error(error.message);
+        res.status(500).send("We are sorry, we ran into an issue!😓");
+    }
+
+})
+
+
 module.exports = router;
