9cba3c488e70359f7fe15591ebcee98e1045d2d6
Harsh Varmora <harsh.vce20@sot.pdpu.ac.in>
2021-10-18 11:52:17 +0530

    added navbar component and react router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import './App.css';
+import{
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import About from './components/About';
 
 function App() {
   return (
       <>
-        <h1>This is Keep Notes</h1>
+      <Router>
+        <Navbar/>
+        <Switch>
+          <Route exact path="/"><Home/></Route>
+          <Route exact path="/about"><About/></Route>
+
+        </Switch>
+      </Router>
       </>
   );
 }
