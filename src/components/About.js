e2c883a787b7253c1333ac086cef754e330c196a
Harsh Varmora <harsh.vce20@sot.pdpu.ac.in>
2021-10-27 14:25:04 +0530

    working with useContext hook using React Context API

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import { useContext, useEffect } from 'react'
+import noteContext from '../context/notes/noteContext'
+
 
 const About = () => {
+    const a = useContext(noteContext)
+
+    useEffect(() => {
+        a.update()
+        // eslint-disable-next-line
+    }, [])
+
     return (
         <div>
-            This is about page
+            This is about {a.state.name} and he is in div {a.state.div}
         </div>
     )
 }
