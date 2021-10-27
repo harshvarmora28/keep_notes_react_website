9cc4b1251e5822fd343ee36738e671dd5e45213e
Harsh Varmora <harsh.vce20@sot.pdpu.ac.in>
2021-10-27 21:00:39 +0530

    using useLocation Hook for marking the page active in navbar

--- src/components/About.js
+++ src/components/About.js
@@ -1,19 +1,12 @@
 import React from 'react'
 import { useContext, useEffect } from 'react'
-import noteContext from '../context/notes/noteContext'
 
 
 const About = () => {
-    const a = useContext(noteContext)
-
-    useEffect(() => {
-        a.update()
-        // eslint-disable-next-line
-    }, [])
 
     return (
         <div>
-            This is about {a.state.name} and he is in div {a.state.div}
+            This is about page
         </div>
     )
 }
--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -3,23 +3,9 @@ import { useState } from "react";
 
 
 const NoteState = (props) => {
-    const s1 = {
-        "name": "Harsh",
-        "div": "1"
-    }
-
-    const [state, setState] = useState(s1)
-    const update = () => {
-        setTimeout(() => {
-            setState({
-                "name": "bittu",
-                "div": "2"
-            })
-        }, 1000);
-    }
 
     return (
-        <NoteContext.Provider value={{state, update}}>
+        <NoteContext.Provider value={{}}>
             {props.children}
         </NoteContext.Provider>
     )
