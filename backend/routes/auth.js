628970a2128e4ae8642e47a1c1200ce1d5bcf6a6
Harsh Varmora <harsh.vce20@sot.pdpu.ac.in>
2021-11-01 15:23:13 +0530

    added Sign in component and redirecting user to Home page if credentials are correct

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -64,6 +64,8 @@ router.post("/login", [
     body("password", "Password cannot be blank").exists()
 ], async (req, res) => {
 
+    let success = false;
+
     // If there are errors return Bad requests and the errors
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
@@ -75,12 +77,14 @@ router.post("/login", [
     try{
         let user = await User.findOne({email});
         if(!user){
-            return res.status(400).json({error: "Incorrect Email or Password!"});
+            success = false;
+            return res.status(400).json({ success, error: "Incorrect Email or Password!"});
         }
         
         const passwordCompare = await bcrypt.compare(password, user.password);
         if(!passwordCompare){
-            return res.status(400).json({error: "Incorrect Email or Password!"});
+            success = false;
+            return res.status(400).json({ success, error: "Incorrect Email or Password!"});
         }
 
         const data = {
@@ -89,7 +93,8 @@ router.post("/login", [
             }
         }
         const authToken = jwt.sign(data, JWT_SECRET);
-        res.json({ authToken });
+        success = true;
+        res.json({ success, authToken });
 
     } catch(error){
         console.error(error.message);
--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import About from "./components/About";
 import NoteState from "./context/notes/NoteState";
+import SignIn from "./components/SignIn";
+import SignUp from "./components/SignUp";
 
 function App() {
   return (
@@ -12,14 +14,20 @@ function App() {
         <Router>
           <Navbar />
           <div className="container">
-          <Switch>
-            <Route exact path="/">
-              <Home />
-            </Route>
-            <Route exact path="/about">
-              <About />
-            </Route>
-          </Switch>
+            <Switch>
+              <Route exact path="/">
+                <Home />
+              </Route>
+              <Route exact path="/about">
+                <About />
+              </Route>
+              <Route exact path="/signin">
+                <SignIn />
+              </Route>
+              <Route exact path="/signup">
+                <SignUp />
+              </Route>
+            </Switch>
           </div>
         </Router>
       </NoteState>
