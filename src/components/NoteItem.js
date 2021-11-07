6d8ba0aa7a7b403c5f7ea03b8cc6bf952f20d42e
Harsh Varmora <harsh.vce20@sot.pdpu.ac.in>
2021-11-07 19:38:20 +0530

    almost done with styling NoteItem component

--- src/components/NoteItem.js
+++ src/components/NoteItem.js
@@ -10,7 +10,7 @@ const NoteItem = (props) => {
 
   return (
     <div className="col-md-4">
-      <div className="card border-0 my-3" style={{borderRadius: "14px"}}>
+      <div className="card border-0 my-3" style={{borderRadius: "14px", backgroundColor: "transparent"}}>
         <div className="card-body" id="noteItem" style={{borderRadius: "12px"}}>
           <h5 className="card-title">{note.title}</h5>
           <p className="card-text">
