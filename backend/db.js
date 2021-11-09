8d9f5ef605401ff124d6ec23ac2a91269bc26a6c
Harsh Varmora <harsh.vce20@sot.pdpu.ac.in>
2021-11-09 22:17:27 +0530

    not able to connect mongodb atlas cluster

--- backend/db.js
+++ backend/db.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 const mongoURI = "mongodb://localhost:27017/KeepNotes?readPreference=primary&appname=MongoDB%20Compass&ssl=false";
 
+
 const connectToMongo = () => {
     mongoose.connect(mongoURI, () => {
         console.log("Connected to mongoDB successfully!");
